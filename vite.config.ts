8f63926ca2ffa2460a2d897f7e9f55c11401de41
Tiago Amorim <tiago.tito@academico.ufpb.br>
2024-03-07 21:23:32 -0300

    x

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "https://titotiago.github.io/conways-game-of-life/",
+  optimizeDeps: {
+    include: ["src/**/*.tsx"],
+  },
 });
